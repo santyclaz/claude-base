e58df743c80bf8728b642d9da0ace81c0d99085b
Andrew Ho <santyclaz@gmail.com>
2026-06-10 22:58:15 -0700

    more properly handling issue with claude + nono policy enforcement errors

---
+++ .devcontainer/utils/post-create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Pre-creating paths since `nono` policy rules can't operate on things that don't exist yet
+# Not doing this in Dockerfile during build since:
+# 1. `/tmp` effectively gets wiped after build since `/tmp` on `debian:trixie` is mounted, in-memory (tmpfs)
+# 2. prefer to keep all the related
+mkdir -p /home/$USER/.docker
+mkdir -p /tmp/claude-$UID
